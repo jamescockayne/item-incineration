5c0ba1e87d5c08001fe4c99a49477b72f597c5c4
James Cockayne <jamescockayne1@gmail.com>
2023-08-13 16:05:17 +0100

    Add setting to toggle incinerator pollution value

--- incinerator.lua
+++ incinerator.lua
@@ -1,3 +1,5 @@
+local realisticPollution = settings.startup["item-incineration-more-realistic-pollution"].value
+
 -- create a new item for incinerator based on electric furnace
 local item = table.deepcopy(data.raw["item"]["electric-furnace"]);
 item.name = "incin-item";
@@ -9,6 +11,12 @@ item.icons = {
 }
 item.place_result = "incin-entity"
 
+local function getEmissionsValue(isPollutionOn)
+    if isPollutionOn then
+        return 15
+    end
+    return 1
+end
 
 -- create an entity that is generated when the item is placed
 local entity = table.deepcopy(data.raw["furnace"]["electric-furnace"]);
@@ -21,7 +29,7 @@ entity.minable = {
 entity.energy_usage = "1800kW"
 entity.energy_source = {
     type = "electric",
-    emissions_per_minute = 10,
+    emissions_per_minute = getEmissionsValue(realisticPollution),
     usage_priority = "secondary-input"
 }
 
--- settings.lua
+++ settings.lua
@@ -10,5 +10,11 @@ data:extend({
         name = "item-incineration-quick-incineration",
         setting_type = "startup",
         default_value = false
+    },
+    {
+        type = "bool-setting",
+        name = "item-incineration-more-realistic-pollution",
+        setting_type = "startup",
+        default_value = true
     }
 })
